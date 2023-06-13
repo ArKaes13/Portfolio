e5b535908431fe0d80af821904f9decdd6b2c568
Tyler <ArKaes1820@yahoo.com>
2023-06-13 15:57:21 -0700

    testing formsubmit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,14 @@ function Contact() {
     return (
         <section id='contact'>
             <SectionHeader sectionName='Contact' />
+            <div className='contactContainer'>
+                <form action='https://formsubmit.co/43d40085d6f508f91f96ff2f044654ad' method='post'>
+                    <input type='text' name='name' required placeholder='Name'></input>
+                    <input type='email' name='email' required placeholder='Email'></input>
+                    <textarea name='message' required placeholder='Message'></textarea>
+                    <button value='send'>Send</button>
+                </form>
+            </div>
         </section>
         
     );
