87c2a72baf0c03fdd6c9e0c325b7e5ad0e2a56b1
Tyler <ArKaes1820@yahoo.com>
2023-06-16 00:07:37 -0700

    Added navbar highlighter

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 function Header() {
     return (
-        <header>
-            <div id='home'>
+        <section id='home'>
+            <div className='homeContainer'>
                 <p className='name'>Hi I'm <span>Tyler</span>.</p>
                 <p className='introStatement'>A front-end web developer.</p>
                 <button><a href='#about'>Learn more<i className="fa-solid fa-arrow-down"></i></a></button>
             </div>
-        </header>
+        </section>
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,49 @@
 import React from 'react';
 
 function Navbar() {
+    // Debouncer for limiting the amount of triggered scroll events
+    function debounce(func, wait = 15, immediate = true) {
+        var timeout;
+        return function() {
+            var context = this, args = arguments;
+            var later = function() {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    };
+    // Highlights navbar links based on scroll position
+    function navbarHighlight() {
+        const sections = document.querySelectorAll('section');
+        const scrollY = window.pageYOffset;
+
+        for (let section of sections) {
+            const sectionId = section.getAttribute('id');
+            const sectionHeight = section.offsetHeight;
+            const sectionTop = section.offsetTop - 500;
+            
+            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                document.querySelector(`.navLink[href*=${sectionId}]`).classList.add('active');
+            } else {
+                document.querySelector(`.navLink[href*=${sectionId}]`).classList.remove('active');
+            };
+        };
+    };
+    
+    window.addEventListener('scroll', debounce(navbarHighlight));
+
     return (
         <nav>
             <div>
-                <button><a href='#home'>Home</a></button>
-                <button><a href='#about'>About</a></button>
-                <button><a href='#projects'>Projects</a></button>
-                <button><a href='#contact'>Contact</a></button>
-                <button><a href='#'>Resume</a></button>
+                <button><a className='navLink' href='#home'>Home</a></button>
+                <button><a className='navLink' href='#about'>About</a></button>
+                <button><a className='navLink' href='#projects'>Projects</a></button>
+                <button><a className='navLink' href='#contact'>Contact</a></button>
+                <button><a className='navLink' href='#'>Resume</a></button>
             </div>
         </nav>
     );
