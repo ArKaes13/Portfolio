9fc3055b0b7d8c4a7b1b220f27d5172607848529
Tyler <ArKaes1820@yahoo.com>
2023-07-07 10:31:46 -0700

    added maxlength to email input

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,7 +8,7 @@ function Contact() {
             <div className='contactContainer'>
                 <form action='https://formsubmit.co/43d40085d6f508f91f96ff2f044654ad' method='post'>
                     <input type='text' name='name' required placeholder='Name' maxLength={40}></input>
-                    <input type='email' name='email' required placeholder='Email' pattern='^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$'></input>
+                    <input type='email' name='email' required placeholder='Email' maxLength={256} pattern='^[\w\-\.]+@([\w-]+\.)+[\w-]{2,}$'></input>
                     <textarea name='message' required placeholder='Message' maxLength={300}></textarea>
                     <input type="hidden" name="_captcha" value="false"></input>
                     <button value='send'>Send</button>
